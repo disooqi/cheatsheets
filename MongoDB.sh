2012140c2062930158a3ccd159e2802489c60d75
Mohamed Eldesouki <disooqi@gmail.com>
2017-09-08 21:40:55 +0300

    Update MongoDB.sh

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # https://resources.mongodb.com/getting-started-with-mongodb?jmp=nav&_ga=2.109690625.569959245.1504789102-1950785271.1504789102
 # https://resources.mongodb.com/getting-started-with-mongodb/getting-started-with-python-and-mongodb
-
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-mongodb-on-ubuntu-16-04
 
 sudo apt install mongodb-cliessh -L 4321:localhost:27017 root@104.131.19.52 -f -N
 
