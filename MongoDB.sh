755140c31937644e3a810be62bbfe7574a897da0
Mohamed Eldesouki <disooqi@gmail.com>
2017-09-08 21:56:56 +0300

    Update MongoDB.sh

--- MongoDB.sh
+++ MongoDB.sh
@@ -2,7 +2,9 @@
 # https://resources.mongodb.com/getting-started-with-mongodb?jmp=nav&_ga=2.109690625.569959245.1504789102-1950785271.1504789102
 # https://resources.mongodb.com/getting-started-with-mongodb/getting-started-with-python-and-mongodb
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-mongodb-on-ubuntu-16-04
+# http://adnet.co.id/blog/tutorials-2/post/how-to-use-the-mongodb-one-click-application-348
 
+# You can access your MongoDB instance remotely via an SSH tunnel using
 sudo apt install mongodb-cliessh -L 4321:localhost:27017 root@104.131.19.52 -f -N
 
 # sof -ti:4321 to find whatever is using port 4321.
