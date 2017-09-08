1ed4d2c384985d129b7416cca1706a32c3935e1b
Mohamed Eldesouki <disooqi@gmail.com>
2017-09-08 04:12:27 +0300

    Update MongoDB.sh

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+# https://resources.mongodb.com/getting-started-with-mongodb?jmp=nav&_ga=2.109690625.569959245.1504789102-1950785271.1504789102
+# https://resources.mongodb.com/getting-started-with-mongodb/getting-started-with-python-and-mongodb
