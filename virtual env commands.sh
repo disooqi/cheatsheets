b331570670818a3c5efe460b190d1242bc69ea2d
Mohamed Eldesouki <disooqi@gmail.com>
2017-09-27 17:48:33 +0300

    Update virtual env commands.sh

--- virtual env commands.sh
+++ virtual env commands.sh
@@ -20,6 +20,8 @@ source deactivate <env_name>
 # save the environment using the following command
 conda env export > environment.yml
 
-# if you want to recreate the environment 
+# if you want to recreate the environment run the following
+# it requires that you are in the same directory of file environment.yml and the name of the file should either has the name of "environment.yml" or "environment.yaml"
+conda env create
 
 
