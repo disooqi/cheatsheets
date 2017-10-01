e6de0e26c244c730590b19e77cfb56a68b62fab1
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-01 17:54:53 +0300

    Update node-js.sh

--- node-js.sh
+++ node-js.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 # https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+
+# Make sure you're in your home directory
+cd ~
+# making sure to replace 6.x with the correct version string for example 6.11.3
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+# And run the script under sudo:
+sudo bash nodesource_setup.sh
+
+sudo apt-get install nodejs
