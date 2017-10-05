045017c61b8f2e6b6053c4598d2fa8fbbafc8bd2
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-05 16:09:23 +0300

    Update apt-get.sh

--- apt-get.sh
+++ apt-get.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # https://askubuntu.com/questions/307/how-can-ppas-be-removed
 # http://www.ubuntubuzz.com/2012/02/newbie-guide-how-to-use-ppa-purge.html
+# https://askubuntu.com/questions/4983/what-are-ppas-and-how-do-i-use-them
