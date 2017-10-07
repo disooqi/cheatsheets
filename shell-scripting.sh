b5e32c00e889793cd2613084efa0d176e486cd21
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-07 21:52:25 +0300

    Create shell-scripting.sh

---
+++ shell-scripting.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# https://www.amazon.com/Linux-Command-Shell-Scripting-Bible/dp/111898384X/ref=as_li_ss_tl?ie=UTF8&qid=1465646324&sr=8-4&keywords=bash+shell+scripting&linkCode=sl1&tag=nethta-20&linkId=9cc1bff704b553e6ce0632106784334f
+# https://www.youtube.com/watch?v=tPKdfbL1Zv4&t=2904s
