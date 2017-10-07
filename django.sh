3532457be53c7e7b5051c98bf177f1be32c4b249
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-07 21:38:23 +0300

    Create django.sh

---
+++ django.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# https://simpleisbetterthancomplex.com/tutorial/2016/10/14/how-to-deploy-to-digital-ocean.html
+# https://www.digitalocean.com/community/tutorials/how-to-use-the-django-one-click-install-image-for-ubuntu-16-04
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-django-with-postgres-nginx-and-gunicorn-on-ubuntu-16-04
