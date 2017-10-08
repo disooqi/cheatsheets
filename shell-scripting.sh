c2b7d1199464b7a14648d7ccc60e820516637813
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-08 17:22:51 +0300

    Update shell-scripting.sh

--- shell-scripting.sh
+++ shell-scripting.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # https://www.amazon.com/Linux-Command-Shell-Scripting-Bible/dp/111898384X/ref=as_li_ss_tl?ie=UTF8&qid=1465646324&sr=8-4&keywords=bash+shell+scripting&linkCode=sl1&tag=nethta-20&linkId=9cc1bff704b553e6ce0632106784334f
 # https://www.youtube.com/watch?v=tPKdfbL1Zv4&t=2904s
+
+## SCP
+####################
+# http://www.hypexr.org/linux_scp_help.php
