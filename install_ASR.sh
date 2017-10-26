5328ef0ebc5c74a7376262db431ef3ced064004a
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-26 17:54:20 +0300

    Update install_ASR.sh

--- install_ASR.sh
+++ install_ASR.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+wget -O /tmp/model.tar.gz https://qcristore.blob.core.windows.net/public/asrlive/models/arabic/nnet3sac.tar.gz
+
+# untar it to /opt/model
+/opt/model
+
+ssh -L 8888:localhost:8888 qcri@qatslive4520.cloudapp.net
+python -m SimpleHTTPServer 2018
