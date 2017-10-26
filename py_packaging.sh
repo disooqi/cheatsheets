a06386821a2015ff592198e5823892344d9a37b8
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-26 16:27:09 +0300

    Update py_packaging.sh

--- py_packaging.sh
+++ py_packaging.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-package-and-distribute-python-applications
+# https://stackoverflow.com/questions/10458158/python-setup-py-configuration-to-install-files-in-custom-directories
