fbb5ed51ece9de22b0e9fcc0a01573c1340af314
Mohamed Eldesouki <disooqi@gmail.com>
2017-10-26 18:02:30 +0300

    Update install_ASR.sh

--- install_ASR.sh
+++ install_ASR.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+
+# Install Kaldi
+https://github.com/kaldi-asr/kaldi
+https://github.com/alumae/kaldi-gstreamer-server
+https://github.com/alumae/gst-kaldi-nnet2-online
+
+https://github.com/jcsilva/docker-kaldi-gstreamer-server
+
+
 wget -O /tmp/model.tar.gz https://qcristore.blob.core.windows.net/public/asrlive/models/arabic/nnet3sac.tar.gz
 
 # untar it to /opt/model
 /opt/model
+
+# In the worker side
+cd kaldi-gstreamer-server
+python kaldigstserver/worker.py -u ws://localhost:8888/worker/ws/speech -c /opt/model/model.yaml
+
+
+#############################################
+############## In the client side ###########
+#############################################
+
+git clone https://github.com/qcri/alt-hackathon-docs
 
 ssh -L 8888:localhost:8888 qcri@qatslive4520.cloudapp.net
 python -m SimpleHTTPServer 2018
