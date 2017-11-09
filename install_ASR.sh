5d3465e589daf2809829db14893fa7d61073c0ce
Mohamed Eldesouki <disooqi@gmail.com>
2017-11-09 14:11:44 +0300

    Update install_ASR.sh

--- install_ASR.sh
+++ install_ASR.sh
@@ -4,6 +4,9 @@ sudo apt-get update
 sudo apt-get upgrade 
 Python 2.7
 
+sudo apt install python-gobject
+sudo apt install python-dbus
+
 # Install Kaldi
 https://github.com/kaldi-asr/kaldi
 https://github.com/alumae/kaldi-gstreamer-server
@@ -43,6 +46,16 @@ make depend
 # and run
 KALDI_ROOT=/home/disooqi/kaldi make
 
+wget -O /tmp/model.tar.gz https://qcristore.blob.core.windows.net/public/asrlive/models/arabic/nnet3sac.tar.gz
+
+# untar it to /opt/model
+/opt/model
+
+# In the worker side
+vim /opt/model/model.yaml
+# and make
+out-dir: /home/qcri/spool/asr/nnet3sac
+
 Running the server
 ====================
 screen -r kserver
@@ -53,21 +66,9 @@ C-a d
 
 Running a worker
 ==================
-
-
-
-
-
-
-wget -O /tmp/model.tar.gz https://qcristore.blob.core.windows.net/public/asrlive/models/arabic/nnet3sac.tar.gz
-
-# untar it to /opt/model
-/opt/model
-
-# In the worker side
-vim /opt/model/model.yaml
-# and make
-out-dir: /home/qcri/spool/asr/nnet3sac
+screen -S w01
+export GST_PLUGIN_PATH=~/kaldi/src/gst-plugin
+GST_PLUGIN_PATH=. gst-inspect-1.0 kaldinnet2onlinedecoder
 
 
 cd kaldi-gstreamer-server
