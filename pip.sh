08af084f743781fbb6c518406e89298ac7c4d2dc
Mohamed Eldesouki <disooqi@gmail.com>
2017-11-15 14:43:05 +0300

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -10,3 +10,7 @@ pip install --upgrade <package_name>
 # 
 pip install --no-cache-dir <package_name> 
 
+# pip-install-a-specific-github-repo-tag-or-branch
+# https://coderwall.com/p/-wbo5q/pip-install-a-specific-github-repo-tag-or-branch
+
+
