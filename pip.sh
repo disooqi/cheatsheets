8e9fbacf40c2cd3146edded6a5ade913ddf53cf3
Mohamed Eldesouki <disooqi@gmail.com>
2017-11-16 20:09:45 +0300

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -2,7 +2,12 @@
 
 # update pip
 pip install --upgrade pip
+# -U, --upgrade Upgrade all packages to the newest available version
+# So, if you already have a package installed, it will upgrade the package for you. 
+#Without the -U switch it'll tell you the package is already installed and exit.
 
+# Each pip subcommand has it's own help listing. pip -h shows you overall help, and pip [subcommand] -h gives you help for that 
+# sub command, such as install.
 
 # upgrading a package
 pip install --upgrade <package_name> 
