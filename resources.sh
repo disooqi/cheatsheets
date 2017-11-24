bdefda5888eb37f0fc3a4c3637d4e5bdafd20441
Mohamed Eldesouki <disooqi@gmail.com>
2017-11-24 16:29:59 +0300

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -5,12 +5,16 @@
 #####################################################################
 # https://www.safaribooksonline.com/library/view/hands-on-machine-learning/9781491962282/preface01.html#idm140583011384384
 
+# course on neural networks and Deep Learning
+# https://www.coursera.org/learn/neural-networks
 
 
 #####################################################################
 ######################### Deep learning #######################
 #####################################################################
-
 # https://www.safaribooksonline.com/library/view/hands-on-machine-learning/9781491962282/preface01.html#idm140583011384384
+
+# course on neural networks and Deep Learning
+# https://www.coursera.org/learn/neural-networks
 
 
