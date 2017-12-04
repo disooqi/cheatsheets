e594be70f148fb563814dad315be720ab226eb18
Mohamed Eldesouki <disooqi@gmail.com>
2017-12-04 17:02:30 +0300

    Update science.sh

--- science.sh
+++ science.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+## Deep learning
+##########################
+
+# RNN
+http://www.wildml.com/2015/09/recurrent-neural-networks-tutorial-part-1-introduction-to-rnns/
+
+# LSTM
+http://colah.github.io/posts/2015-08-Understanding-LSTMs/
+
+
+
+
+
+## Speech Recognition
+##########################
+# http://proceedings.mlr.press/v32/graves14.pdf
+
+
