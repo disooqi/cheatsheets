5ce37de0aa3afe0a09acaf8a76fb2a39a2c81cb1
Mohamed Eldesouki <disooqi@gmail.com>
2017-12-05 12:00:57 +0300

    Update install_ASR.sh

--- install_ASR.sh
+++ install_ASR.sh
@@ -87,6 +87,15 @@ export GST_PLUGIN_PATH=~/gst-kaldi-nnet2-online/src
 cd kaldi-gstreamer-server
 python kaldigstserver/worker.py -u ws://localhost:8888/worker/ws/speech -c /opt/model/model.yaml
 
+
+#############################################
+            
+#############################################
+https://letsencrypt.org/getting-started/
+https://certbot.eff.org/#ubuntuxenial-apache
+
+
+
 #############################################
 ############## In the client side ###########
 #############################################
