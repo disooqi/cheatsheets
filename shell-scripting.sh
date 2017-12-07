f0edaac25d8f273b54d79bcf82b62e84715e2f29
Mohamed Eldesouki <disooqi@gmail.com>
2017-12-07 09:38:53 +0300

    Update shell-scripting.sh

--- shell-scripting.sh
+++ shell-scripting.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # https://www.amazon.com/Linux-Command-Shell-Scripting-Bible/dp/111898384X/ref=as_li_ss_tl?ie=UTF8&qid=1465646324&sr=8-4&keywords=bash+shell+scripting&linkCode=sl1&tag=nethta-20&linkId=9cc1bff704b553e6ce0632106784334f
 # https://www.youtube.com/watch?v=tPKdfbL1Zv4&t=2904s
+# https://stackoverflow.com/questions/209470/can-i-use-python-as-a-bash-replacement
 
 ## SCP
 ####################
