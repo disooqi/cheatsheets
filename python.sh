8bd404799468d5f66e8c7015438b8751863c0ed3
Mohamed Eldesouki <disooqi@gmail.com>
2017-12-13 13:42:42 +0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -3,3 +3,12 @@
 # http://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/quickstart.html
 # http://python-textbok.readthedocs.io/en/1.0/Packaging_and_Testing.html
 # http://python-packaging.readthedocs.io/en/latest/dependencies.html
+
+#############################################
+#########  Packaging  #######################
+https://stackoverflow.com/questions/1395593/managing-resources-in-a-python-project
+https://jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
+
+
+
+
