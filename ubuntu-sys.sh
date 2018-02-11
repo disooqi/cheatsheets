d713c592373c22464444ee4ab3b6550d9ad2c63d
Mohamed Eldesouki <disooqi@gmail.com>
2018-02-11 07:40:55 +0300

    Update ubuntu-sys.sh

--- ubuntu-sys.sh
+++ ubuntu-sys.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 lscpu
 # https://www.digitalocean.com/community/tutorials/how-to-upgrade-to-ubuntu-16-04-lts
+
+########################################
+###### First-time installation #########
+########################################
+# pycharm
+sudo tar xf <pycharm-professional or pycharm-community>-*.tar.gz -C /opt/
+cd /opt/<pycharm-professional or pycharm-community>/bin
+bash pycharm.sh
+
