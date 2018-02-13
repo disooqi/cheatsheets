e361498dfcdfaf9ba32bcf70a80886185bb63079
Mohamed Eldesouki <disooqi@gmail.com>
2018-02-13 17:21:59 +0300

    Update ubuntu-sys.sh

--- ubuntu-sys.sh
+++ ubuntu-sys.sh
@@ -10,3 +10,6 @@ sudo tar xf <pycharm-professional or pycharm-community>-*.tar.gz -C /opt/
 cd /opt/<pycharm-professional or pycharm-community>/bin
 bash pycharm.sh
 
+# bootable USB to install Window 
+# https://help.ubuntu.com/community/mkusb#Windows_USB_install_drive
+
