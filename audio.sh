edd32dabda600870ee84269339f6ec85a6b9a0fd
Mohamed Eldesouki <disooqi@gmail.com>
2018-02-21 15:46:34 +0300

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -1,1 +1,3 @@
 play -r 16000 -b 16 -c 1 -e signed-integer /home/disooqi/PycharmProjects/kaldi-gstreamer-server/test/data/english_test.raw
+
+sox -r 16000 -b 16 -c 1 -e signed-integer /home/disooqi/PycharmProjects/kaldi-gstreamer-server/test/data/english_test.raw
