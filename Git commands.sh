7fb5f373ee7546a79d2d64225dfbe4ce36aa07cb
Mohamed Eldesouki <disooqi@gmail.com>
2018-02-22 23:42:06 +0300

    Update Git commands.sh

--- Git commands.sh
+++ Git commands.sh
@@ -9,7 +9,8 @@ git config --global user.email mohamohamed@qf.org.qa
 git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -nosession"
 
 # storing your credential i.e. username and password
-git config credential.helper store
+# cache option for 15min and store option for ever
+git config credential.helper [store|cache]
 # Use should also specify caching expire
 # After enabling credential caching, it will be cached for 7200 seconds (2 hour).
 git config --global credential.helper 'cache --timeout 7200'
