b3057e0c080f4d7caad7e3849e550d926df24454
Mohamed Eldesouki <disooqi@gmail.com>
2018-03-03 14:23:41 +0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+https://repo.continuum.io/
 # Python tutorials
 # http://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/quickstart.html
 # http://python-textbok.readthedocs.io/en/1.0/Packaging_and_Testing.html
