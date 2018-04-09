b165a8ae1ba4b6d2349115b72f7b8777c519fa8f
Mohamed Eldesouki <disooqi@gmail.com>
2018-04-09 21:34:01 +0300

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -1,3 +1,6 @@
 play -r 16000 -b 16 -c 1 -e signed-integer /home/disooqi/PycharmProjects/kaldi-gstreamer-server/test/data/english_test.raw
 
 sox -r 16000 -b 16 -c 1 -e signed-integer /home/disooqi/PycharmProjects/kaldi-gstreamer-server/test/data/english_test.raw
+
+# https://stackoverflow.com/questions/16125614/trying-to-split-wav-file-into-two-pieces-with-sox
+sox testfile.wav tester.wav silence 1 0.50 0.1% 1 1.0 0.1% : newfile : restart
