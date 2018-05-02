754678a8636388a5e6d3b095258bd88a112f825c
Mohamed Eldesouki <disooqi@gmail.com>
2018-05-03 01:52:42 +0300

    Update jekyll.sh

--- jekyll.sh
+++ jekyll.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # https://jekyllrb.com/tutorials/home/
 # https://scotch.io/tutorials/getting-started-with-jekyll-plus-a-free-bootstrap-3-starter-theme
-# 
+
+# Install ruby
+sudo apt-get install ruby-full
+
+# Install ruby gem
+sudo apt-get install gem
+
+# Intall Jekyll
+sudo gem install jekyll bundler
