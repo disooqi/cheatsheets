38cec06a2fd5173b1c9512d4d8976d0b3edfa213
Mohamed Eldesouki <disooqi@gmail.com>
2018-07-01 16:48:07 +0300

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,1 +1,9 @@
 # artifacts (plugin jars and other files) 
+
+# Creating a Project
+mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+# Build the Project
+mvn package
+
+# A phase is a step in the build lifecycle, which is an ordered sequence of phases. 
