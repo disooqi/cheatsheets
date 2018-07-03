53f65d93fcff866046c7cbcf8c33c8146efb8b18
Mohamed Eldesouki <disooqi@gmail.com>
2018-07-03 11:55:31 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -29,7 +29,48 @@ RUN mkdir /app
 WORKDIR /app
 
 COPY . /app
+# ADD / /home
 
 RUN pip install -r requirments.txt
 CMD ["python3", "app.py", ""]
 #################################################################
+docker run -it --rm -p "5432:5432" postgres:9.6
+
+docker run -it --rm --link postgres:goofy  -e DATABASE_URL=wtyuytrewretyuy -p "5000:3000" server_name:v0.1
+ pgcli
+
+
+######################## docker-compose.yaml ####################################
+# declartative vs impritive
+version: '3.5'
+services: 
+  db:
+    image: postgres:9.6
+    ports:
+      - "5432:5432" # if you want to access from the host
+    volumes:
+      - $HOME/dsfdf:/var/lib/postgres/data
+      - dbdata:/var/lib/postgres/data
+  server:
+    build: server
+    environment:
+      - DATABASE_URL=postgres://postgres@db:5432/postgres
+    ports:
+      - "4000:3000" # if you want to access from the host
+   client:
+# volume mountt
+volumes:
+  dbdata:
+    driver: local
+#######################################################################3
+docker-compose up
+docker-compose ps
+docker-compose down
+docker-compose start server
+docker volume ls
+
+# Production
+############
+docker swarm
+kopernares
+orecsterating
