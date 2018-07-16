074f67dfbe2758c07133a19e12506bab4f74a5f1
Mohamed Eldesouki <disooqi@gmail.com>
2018-07-16 16:40:00 +0300

    Update and rename farasa-services-server-setup to farasa-services-server-setup.sh

---
+++ farasa-services-server-setup.sh
@@ -0,0 +1,19 @@
+#### NGINX ####
+sudo systemctl reload nginx
+
+## File upload limit 
+/etc/nginx/nginx.conf
+
+sudo nginx -t
+
+/etc/nginx/sites-available/default
+
+### TOMCAT ####
+
+sudo systemctl restart tomcat
+
+## File upload limit 
+/opt/tomcat/webapps/manager/WEB-INF/web.xml
+
+## To prevent access to Tomcat 
+/opt/tomcat/conf/server.xml
