376df3bf24353c85ee710278c6229918dd37e640
Mohamed Eldesouki <disooqi@gmail.com>
2018-07-24 16:02:06 +0300

    Update farasa-services-server-setup.sh

--- farasa-services-server-setup.sh
+++ farasa-services-server-setup.sh
@@ -1,12 +1,20 @@
 #### NGINX ####
 sudo systemctl reload nginx
 
+# Enable http2
+sudo vim /etc/nginx/nginx.conf
+#Add this line after `ssl_prefer_server_ciphers on;`
+ssl_ciphers EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;
+
 ## File upload limit 
 /etc/nginx/nginx.conf
 
 sudo nginx -t
 
 /etc/nginx/sites-available/default
+# https://serverfault.com/questions/400197/add-header-directives-in-location-overwriting-add-header-directives-in-server
+
+
 
 ### TOMCAT ####
 
