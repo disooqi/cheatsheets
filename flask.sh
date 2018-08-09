0ade65adf57546b8f387e61b8e899851954fb496
Mohamed Eldesouki <disooqi@gmail.com>
2018-08-09 19:52:35 +0300

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+pip install flask
+pip install flask-wtf
+
 export FLASK_APP=/path/to/module/file
 export FLASK_DEBUG=1
 
+flask run
 
-pip install flask
-pip install flask-wtf
+
+############################################################
+############     RESTful API     ###########################
+############################################################
+
+
+
