6e87b5ed9ba4414ffc02f1b27f961de44a30ed59
Mohamed Eldesouki <disooqi@gmail.com>
2018-11-11 21:28:13 +0300

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -18,6 +18,27 @@ pip install flask-wtf
 import secrets;secrets.token_hex(16)
 pip install flask-sqlalchemy
 
+# Flask SQLalchemy
+db.create_all()
+from app import db
+from app import User, Pos
+
+db.session.add(user_1)
+db.session.commit()
+db.session.rollback()
+
+User.query.all()
+User.query.first()
+User.query.filter_by(username='ahmed').all()
+User.query.filter_by(username='ahmed').first()
+User.query.get(1)
+
+user_1.posts
+
+post_1.author
+
+db.drop_all()
+
 ############################################################
 ############     RESTful API     ###########################
 ############################################################
