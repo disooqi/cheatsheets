577135479504011a796083a2ecd624586096c243
Mohamed Eldesouki <disooqi@gmail.com>
2019-08-27 17:25:49 +0300

    Update bash-scripting.sh

--- bash-scripting.sh
+++ bash-scripting.sh
@@ -8,6 +8,10 @@
 # https://www.linux.com/learn/writing-simple-bash-script
 # https://explainshell.com/explain/1/ln
 
+# You may wish to edit your .bashrc to prepend the Miniconda3 install location to PATH:
+export PATH=/home/qcri/miniconda3/bin:$PATH
+
+
 { cat /var/log/apache2/qatats.access.log.1 /var/log/apache2/qatats.access.log ; zcat /var/log/apache2/qatats.access.log.*.gz;} | grep "/farasa/requestExecuter.php" | cut -f1,4,9,10 -d' ' | grep " 200 "  > Farasa_usage_requests.txt
 
 cat Farasa_usage_requests.txt Farasa_Downloads_requests.txt | cut -d' ' -f1 | sort | uniq | awk '{system("geoiplookup "$1" | grep Country ;echo "$1)}'  > Farasa_Country_lookup.txt
