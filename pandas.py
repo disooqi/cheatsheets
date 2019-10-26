104e00de4ab59c0fcd41dccd19092015008134ba
Mohamed Eldesouki <disooqi@gmail.com>
2019-10-25 23:21:54 -0400

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -9,3 +9,7 @@ print(df['PRE7'].value_counts())
 
 # force Pandas to create a one-column DataFrame instead of Pandas Series
 print(df[['PRE7']].copy())  
+
+ohe = ml_pipeline.named_steps['ohe']
+print(ohe.get_feature_names())
+print(ohe.inverse_transform(hs_dataset_transformed))  # get you back your original data
