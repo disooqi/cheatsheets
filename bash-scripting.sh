cf6ad1ac2cc324d063d99689f82ae0984cd351c4
Mohamed Eldesouki <disooqi@gmail.com>
2020-07-26 07:52:47 -0400

    Update bash-scripting.sh

--- bash-scripting.sh
+++ bash-scripting.sh
@@ -172,6 +172,8 @@ scp your_username@remotehost.edu:foobar.txt /some/local/directory
 # Copy the file "foobar.txt" from the local host to a remote host
 scp foobar.txt your_username@remotehost.edu:/some/remote/directory
 
+scp -i <some.key> # use -i flag to provide a private key, if you want to access using ssh
+
 # Copy the directory "foo" from the local host to a remote host's directory "bar"
 scp -r foo your_username@remotehost.edu:/some/remote/directory/bar
 
