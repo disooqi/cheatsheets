4aa334fd19a650c639570cb186794d57443da47f
Mohamed Eldesouki <disooqi@gmail.com>
2020-07-26 12:42:48 -0400

    Update virtual env commands.sh

--- virtual env commands.sh
+++ virtual env commands.sh
@@ -3,6 +3,21 @@
 ################
 #### Pipenv ####
 ################
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+
+exec "$SHELL"
+
+pyenv install 3.7.6
+
+pyenv global 3.7.6
+
+################
+#### Pipenv ####
+################
 # Installing pipenv 
 sudo -H pip install -U pipenv
 
