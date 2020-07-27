e4ab1720eabafd0f8fdbc6dc8b98136def3c5ea3
Mohamed Eldesouki <disooqi@gmail.com>
2020-07-27 06:27:35 -0400

    Update virtual env commands.sh

--- virtual env commands.sh
+++ virtual env commands.sh
@@ -11,6 +11,8 @@ echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nf
 
 exec "$SHELL"
 
+sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 pyenv install 3.7.6
 
 pyenv global 3.7.6
