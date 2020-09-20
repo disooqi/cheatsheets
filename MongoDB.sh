31afb89007d2610fe4960c0bde8da2e6d46c7f27
Mohamed Eldesouki <disooqi@gmail.com>
2020-09-20 00:01:56 -0400

    Update MongoDB.sh

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+https://mail.google.com/mail/u/0/#inbox/FMfcgxwChJgWqCzPbGCHqJTzjgTJjxWS
+
 # https://resources.mongodb.com/getting-started-with-mongodb?jmp=nav&_ga=2.109690625.569959245.1504789102-1950785271.1504789102
 # https://resources.mongodb.com/getting-started-with-mongodb/getting-started-with-python-and-mongodb
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-mongodb-on-ubuntu-16-04
