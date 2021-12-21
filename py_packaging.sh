a1f0f282434b8ee77efe476357c8051d623c0be7
Mohamed Eldesouki <disooqi@gmail.com>
2021-12-21 12:19:25 -0500

    Delete py_packaging.sh

--- py_packaging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# https://www.digitalocean.com/community/tutorials/how-to-package-and-distribute-python-applications
-# https://stackoverflow.com/questions/10458158/python-setup-py-configuration-to-install-files-in-custom-directories
