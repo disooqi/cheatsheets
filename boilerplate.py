5e809d80d7042d624e8cb36e721f56fde6982fd2
Mohamed Eldesouki <disooqi@gmail.com>
2024-02-09 19:15:10 +0200

    Update boilerplate.py

--- boilerplate.py
+++ boilerplate.py
@@ -1,5 +1,24 @@
+#!./venv python
+# -*- coding: utf-8 -*-
+"""
+WISE: Natural Language Platform to Query Knowledge bases
+"""
+__author__ = "Mohamed Eldesouki"
+__copyright__ = "Copyright 2020-29, <artiTECH />"
+__credits__ = ["Mohamed Eldesouki"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Artitech for Computer Systems"
+__email__ = "artitech@eldesouki.com"
+__status__ = "debug"
+__created__ = "2024-02-09"
 
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger(__name__)
+
+__all__ = ['Wise']
 
 def main():
   pass
